830696aa35c478f76313739f22d25fe2dc813350
Minjoo Sur <loveshero92@gmail.com>
2017-12-31 02:25:30 -0800

    json file format editted to list

--- testData/app/controllers/data_controller.rb
+++ testData/app/controllers/data_controller.rb
@@ -1,6 +1,6 @@
 class DataController < ApplicationController
   def get
-	  data = {:id => "1", :name => "Hand & Body Lotion", :brand => "The Naked Bee", :image => "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=the+naked+bee"}
+	  data = {{:id => "1", :name => "Hand & Body Lotion", :brand => "The Naked Bee", :image => "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=the+naked+bee"}}
 	  render :json => data
   end
 end
